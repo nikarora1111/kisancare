1e172b84d7fb7c81bde8a5bb3ddf9bbe1c3c1f55
niks <nikarora1111@gmail.com>
2019-03-16 03:58:21 +0530

    added toast pop up

--- app/src/main/java/com/hackfest/kisancare/MainActivity.java
+++ app/src/main/java/com/hackfest/kisancare/MainActivity.java
@@ -2,9 +2,23 @@ package com.hackfest.kisancare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void clickFunction(View view){
+        EditText name = (EditText) findViewById(R.id.Name);
+        EditText number = (EditText) findViewById(R.id.Mobile);
+        EditText state = (EditText) findViewById(R.id.state);
+        EditText dist = (EditText) findViewById(R.id.district);
+        EditText area = (EditText) findViewById(R.id.Name);
+        Log.i("name", name.getText().toString());
+        Log.i("number", number.getText().toString());
+        Toast.makeText(MainActivity.this, "Hi There!"+name, Toast.LENGTH_SHORT).show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
